3b2acfbb22bed68629d94dc31a2ea5c8b64206f9
Josip S <josip.skrlecvk@gmail.com>
2023-09-07 20:16:43 +0200

    pretvaranje u JSON

--- .vscode/javascript/7.9objekti.js
+++ .vscode/javascript/7.9objekti.js
@@ -14,3 +14,20 @@ console.log(dog.bark());
 
 dog.breed = "Pekinezer";
 console.log(dog.breed);
+/*D.Z. za pročitati https://medium.com/@shrijan00003/
+javascript-object-properties-writable-enumerable-
+configurable-eced27378256*/
+
+var igracka = dog["has Toy"];
+console.log(igracka);
+
+cat.color = "grey";
+cat.size = 30;
+console.log(cat);
+
+//delete dog.bark;
+
+var ispisObjekta = JSON.stringify(dog);
+//pretvaranje u JSON objekt
+console.log(dog);
+console.log(ispisObjekta);
