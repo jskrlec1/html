b086111a4caad4a8655b5a6128625bdced283cb4
Josip S <josip.skrlecvk@gmail.com>
2023-09-07 20:19:02 +0200

    pretvaranje u JSON

--- .vscode/javascript/7.9objekti.js
+++ .vscode/javascript/7.9objekti.js
@@ -25,9 +25,9 @@ cat.color = "grey";
 cat.size = 30;
 console.log(cat);
 
-//delete dog.bark;
+delete dog.bark;
 
 var ispisObjekta = JSON.stringify(dog);
 //pretvaranje u JSON objekt
-console.log(dog);
+console.log();
 console.log(ispisObjekta);
